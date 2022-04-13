cb7ef43c2e483bf02b7d7a3da98e18840577883d
pawan-malhotra <papita.malhotra1997@gmail.com>
2022-04-13 22:38:34 +0530

    initial setup

---
+++ src/test/java/com/team33/evotingsystem/EVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team33.evotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
